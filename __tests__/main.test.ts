60d7c31f609461c0bd627bd5e1e49455e0a62a19
temachy <temuchin26@gmail.com>
2024-05-30 11:09:25 +0300

    run bundle npm command

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -15,7 +15,7 @@ const runMock = jest.spyOn(main, 'run')
 // Mock the GitHub Actions core library
 // let debugMock: jest.SpiedFunction<typeof core.debug>
 let errorMock: jest.SpiedFunction<typeof core.error>
-let getInputMock: jest.SpiedFunction<typeof core.getInput>
+// let getInputMock: jest.SpiedFunction<typeof core.getInput>
 // let setFailedMock: jest.SpiedFunction<typeof core.setFailed>
 let setOutputMock: jest.SpiedFunction<typeof core.setOutput>
 
@@ -25,21 +25,21 @@ describe('action', () => {
 
     // debugMock = jest.spyOn(core, 'debug').mockImplementation()
     errorMock = jest.spyOn(core, 'error').mockImplementation()
-    getInputMock = jest.spyOn(core, 'getInput').mockImplementation()
+    // getInputMock = jest.spyOn(core, 'getInput').mockImplementation()
     // setFailedMock = jest.spyOn(core, 'setFailed').mockImplementation()
     setOutputMock = jest.spyOn(core, 'setOutput').mockImplementation()
   })
 
   it('sets the time output', async () => {
     // Set the action's inputs as return values from core.getInput()
-    getInputMock.mockImplementation(name => {
-      switch (name) {
-        case 'milliseconds':
-          return '500'
-        default:
-          return ''
-      }
-    })
+    // getInputMock.mockImplementation(name => {
+    //   switch (name) {
+    //     case 'milliseconds':
+    //       return '500'
+    //     default:
+    //       return ''
+    //   }
+    // })
 
     await main.run()
     expect(runMock).toHaveReturned()
