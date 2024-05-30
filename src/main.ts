f8a679c3529d9baa15ece581171cfd192fa4baac
temachy <temuchin26@gmail.com>
2024-05-30 13:13:41 +0300

    add backend service url to .env, make request to run workflow, log env variables

--- src/main.ts
+++ src/main.ts
@@ -13,12 +13,39 @@ export async function run(): Promise<void> {
       core.getInput('parameters') || '{}'
     )
 
+    const workflowRunURL = `${process.env.API_BASE_URL}/workflow/${workflowId}/run`
+    let response
+    try {
+      const rawResponse = await fetch(workflowRunURL, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${chassyToken}`
+        },
+        body: JSON.stringify({
+          githubData: {
+            envContext: {},
+            githubContext: {}
+          },
+          dryRun: true
+        })
+      })
+      if (!rawResponse.ok) {
+        throw new Error('Network response was not ok ' + rawResponse.statusText)
+      }
+      response = rawResponse.json()
+    } catch (e) {
+      console.debug(`Error during making POST request to ${workflowRunURL}`)
+      if (e instanceof Error) core.setFailed(e.message)
+    }
+
+    console.log('response', response)
     console.log('workflowId', workflowId)
     console.log('chassyToken', chassyToken)
     console.log('parameters', parameters)
-    console.log('github.context', github.context)
+    console.log('github.context', github)
     // Set outputs for other workflow steps to use
-    core.setOutput('workflowExecution', 'workflowExecution will be here')
+    core.setOutput('workflowExecution', JSON.stringify(response))
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
